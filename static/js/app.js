00c89a391781c98a21eb35f53ea7d2868b115c4e
Katiusca <quijadakatiusca@gmail.com>
2020-12-13 21:55:47 -0500

    Challenge

--- static/js/app.js
+++ static/js/app.js
@@ -37,7 +37,7 @@ function updateFilters() {
     // 4c. Save the id of the filter that was changed as a variable.
   let filterId = changedElement.attr("id");
   console.log(filterId);
-  
+    
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
   if (elementValue) {
@@ -45,7 +45,7 @@ function updateFilters() {
   }
   else {
     delete filters[filterId];
-  }
+  };
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -63,18 +63,40 @@ function updateFilters() {
       filteredData = filteredData.filter(row => row[key] === value);
     });   
 
+    //My recommendation #3 "Confirmation message that the search is not found"
+    if (filteredData.length === 0) {
+      document.getElementById("notFound").innerHTML = "Sorry, search not found!";
+      document.getElementById("notFound2").innerHTML = "-Try changing your search criteria or remove filters";
+      }
+    else {
+      document.getElementById("notFound").innerHTML = "";
+      document.getElementById("notFound2").innerHTML = "";
+    };
+
     // 10. Finally, rebuild the table using the filtered data
     buildTable(filteredData);
   };
 
+
+  // My recommendations
+  // Recommendation #1 "Reset Search button"
   function resetFilters() {
     location.reload();
-  }
+  };
   
+
+  // Recommendation #2 To convert to lower case even if writing is ALL CAPITAL
+  function forceLower(strInput) {
+    strInput.value = strInput.value.toLowerCase();
+  }
+
+
+
   // 2. Attach an event to listen for changes to each filter
   d3.selectAll("input").on("change", updateFilters);
-  d3.select("#reset").on("click", resetFilters); 
-
+  d3.select("#reset").on("click", resetFilters);
+    
 
   // Build the table when the page loads
   buildTable(tableData);
+
